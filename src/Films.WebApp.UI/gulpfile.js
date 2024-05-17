80aa041fa3197a63f1c665f2c490d53f8e592028
andyvernon <andy_vernon@live.co.uk>
2024-05-17 22:30:27 +0100

    Added browserfy, gulp and others

--- src/Films.WebApp.UI/gulpfile.js
+++ src/Films.WebApp.UI/gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require("gulp");
 var browserify = require("browserify");
 var source = require("vinyl-source-stream");
 var tsify = require("tsify");
+var sourcemaps = require("gulp-sourcemaps");
+var buffer = require("vinyl-buffer");
 var paths = {
     pages: ["src/*.html"]
 };
@@ -11,16 +13,27 @@ gulp.task("copy-html", function () {
         .pipe(gulp.dest("dist"));
 });
 
-gulp.task("default", gulp.series(gulp.parallel("copy-html"), function () {
-    return browserify({
-        basedir: ".",
-        debug: true,
-        entries: ["src/main.ts"],
-        cache: {},
-        packageCache: {}
-    })
-        .plugin(tsify)
+gulp.task(
+    "default", 
+    gulp.series(gulp.parallel("copy-html"), function () {
+        return browserify({
+            basedir: ".",
+            debug: true,
+            entries: ["src/main.ts"],
+            cache: {},
+            packageCache: {}
+        })
+            .plugin(tsify)
+            .transform("babelify", {
+                presets: ["es2015"],
+                extensions: [".ts"]
+        })
         .bundle()
         .pipe(source("bundle.js"))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({
+            loadMaps: true
+        }))
+        .pipe(sourcemaps.write("./"))
         .pipe(gulp.dest("dist"));
 }));
